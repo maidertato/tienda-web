8cd80d98c2be88d79666f1cb3175eadc5bb01ac1
Ane <ane@email.com>
2026-04-06 22:16:55 +0200

    firebase prueas

--- servidor/index.js
+++ servidor/index.js
@@ -3,17 +3,22 @@ const cors = require('cors');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo').default;
+const path = require('path');
+
 const app = express();
 
 
 const misProductos = require('./rutas/productos');
 const misUsuarios = require('./rutas/usuarios');
-// Middlewares
+
 app.use(cors({
-    origin: 'http://localhost:3000', // El puerto de React
-    credentials: true                // Para  las sesiones
+    origin: 'http://localhost:3000', // El puerto de tu React (cliente)
+    credentials: true
 }));
-app.use(express.json()); 
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Aquí conectamos Express con la base de datos 'tienda'
 app.use(session({
@@ -38,13 +43,15 @@ app.use((req, res, next) => {
     next();
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/tienda')
-    .then(() => console.log('Conectado a MongoDB (Base de datos: tienda)'))
-    .catch(err => console.error('Error al conectar a MongoDB:', err));
+
+//  Conectar a MongoDB
+// mongoose.connect('mongodb://127.0.0.1:27017/tienda')
+//    .then(() => console.log('Conectado a MongoDB (Base de datos: tienda)'))
+//    .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 // usar rutas
-app.use('//productos', misProductos);
-app.use('//usuarios', misUsuarios);
+app.use('/productos', misProductos);
+app.use('/usuarios', misUsuarios);
 
 app.get('/', (req, res) => {
     res.send('Servidor de la Tienda de mascotas funcionando correct');
@@ -56,7 +63,8 @@ app.get('/api/contador', (req, res) => {
 });
 
 // Puerto
-const PORT = 5001;
+const PORT = 5002;
 app.listen(PORT, () => {
     console.log(`Servidor en http://localhost:${PORT}`);
+    console.log(`Firebase HTML en: http://localhost:${PORT}/email-password.html`);
 });
